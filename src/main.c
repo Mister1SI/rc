3c7bf3b82d9bb572ae1c583721a5469607320d0b
Mister1SI <wazzup2690@gmail.com>
2024-01-06 20:43:50 -0800

    Store tmp file in /tmp/.rc-tmp

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <preproccessor.h>
 
+#include <sys/file.h>
+#include <sys/types.h>
 
 int main(int argc, char* argv[]) {
 	if(argc < 3) {
@@ -8,14 +10,39 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	char* filename = argv[1];
-	char* new_filename = argv[2];
+	char* destination = argv[2];
+	char* tmp_filename = "/tmp/.rc-tmp"; // 12 chars
 
-	int preproccessor_result = preproccessor(filename, new_filename);
+	int preproccessor_result = preproccessor(filename, tmp_filename);
 	if(preproccessor_result != 0) {
 		char* msg = "Preproccessor Error\n";
 		write(1, msg, 20);
 		return 1;
 	}
+
+	int tmp_fd = open(tmp_filename, O_RDONLY, 00644);
+	if(tmp_fd == -1) {
+		char* msg = "File error\n";
+		write(1, msg, 11);
+		return 1;
+	}
+	
+	struct stat st;
+	fstat(tmp_fd, &st);
+	long tmp_filesize = st.st_size;
+
+	char* filebuf = malloc(tmp_filesize);
+	read(tmp_fd, filebuf, tmp_filesize);
+	close(tmp_fd);
+
+	int new_fd = open(destination, O_CREAT | O_WRONLY, 00644);
+	if(new_fd == -1) {
+		write(1, "File error\n", 11);
+		return 1;
+	}
+
+	write(new_fd, filebuf, tmp_filesize);
+	close(new_fd);
 
 	return 0;
 }

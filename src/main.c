0a9c816de89f85e60564b2d2bca0d40d839716ee
Mister1SI <74750011+Mister1SI@users.noreply.github.com>
2024-01-06 18:15:45 -0800

    Reformat main.c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,15 @@
 
 
 int main(int argc, char* argv[]) {
+	if(argc < 3) {
+		char* msg = "Please supply at least 2 arguments\n";
+		write(1, msg, 35);
+		return 1;
+	}
+	char* filename = argv[1];
+	char* new_filename = argv[2];
+
+	preproccessor(filename, new_filename);
 	
 	return 0;
 }

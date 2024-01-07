bb57b9b65ff35d7b01509313d04c18f3b83b25cd
Mister1SI <wazzup2690@gmail.com>
2024-01-06 20:09:54 -0800

    Began work on preproccessor as function

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-gcc -Wno-implicit-function-declaration src/main.c src/preproccessor.c -o main
+gcc -Wno-implicit-function-declaration -I./inc src/main.c src/preproccessor.c -o main
 ./main $1 $2
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <preproccessor.h>
+
+
+int main(int argc, char* argv[]) {
+	
+	return 0;
+}
+
